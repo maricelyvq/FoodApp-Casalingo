c83c170e3f6de1f8033255160d46da56cd0bce4a
Maricely Villalobos <maricely.villalobos21@gmail.com>
2025-03-23 19:27:56 -0500

    connecting Mongo DB: creating connetion .env file, adding it to backend index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
-import express,{Request,Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
 const port = process.env.PORT || 7000;
 
-
+//database connection
+mongoose.connect(process.env.DATABASE_URL as string)
+    .then(() => console.log("Database connected"));
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async  (req: Request, res:Response) => {
-    res.json({message: "Hello World!"});
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
