f0b31f5453f37a99b264a3a61348f9a61e7b6c51
maricelyvq <maricely.villalobos21@gmail.com>
2025-03-31 18:37:02 -0500

    Nav (#9)
    * User profile update feature"
    
    * User profile updating

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,7 +1,8 @@
 
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse} from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 
 const router = express.Router();
@@ -10,6 +11,12 @@ const router = express.Router();
 //api/my/user
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-
+router.put(
+    "/",
+    jwtCheck,
+    jwtParse,
+    validateMyUserRequest,
+    MyUserController.updateCurrentUser
+  );
 
 export default router;
